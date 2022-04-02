d7b8b2ce29eb25875ce995b491521d12b7907643
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2022-04-02 20:03:35 +0100

    update

---
+++ rpg-inventory/demo/inventory/InventoryManagerItem.gd
@@ -0,0 +1,14 @@
+# List of creted items for InventoryManger to use in source code: MIT License
+# @author Vladimir Petrenko
+tool
+class_name InventoryManagerItem
+
+const WOOD = "23896aa9-d342-4e7d-9cb1-18583cb6d360"
+const RECIPEHELMETKNIGHT2D = "97b7c8ba-3c27-4158-9633-11dbd4fefd21"
+const RECIPEHELMETVIKING2D = "73cbb965-26a5-4867-9849-9909c096641f"
+
+const ITEMS = [
+ "Wood",
+ "RecipeHelmetKnight2D",
+ "RecipeHelmetViking2D"
+]
