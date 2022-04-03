38b72b46c27969236b575f48c87bf5dbee43fa72
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2022-04-03 12:18:50 +0100

    add animations

--- behaviour-tree/godot-project/world_objects/villager/Villager.gd
+++ behaviour-tree/godot-project/world_objects/villager/Villager.gd
@@ -1,2 +1,22 @@
+tool
 class_name Villager
 extends KinematicBody2D
+
+enum {
+	RUN,
+	IDLE,
+	CARRY,
+	CHOPPING,
+	WATERING,
+	DOING,
+	DIGGING
+}
+
+onready var animation_tree:BlendPositionAnimationTree = $AnimationTree
+
+func _ready():
+	animation_tree.active = true
+	
+func _process(delta):
+	animation_tree.position = Vector2(-10, 0)
+	animation_tree.transition = DIGGING
