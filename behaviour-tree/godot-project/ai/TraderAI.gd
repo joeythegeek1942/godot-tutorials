9468a7e053294c5a907ac9a287776a9c6c894082
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2022-06-09 07:10:14 +0100

    Change project

--- behaviour-tree/godot-project/ai/TraderAI.gd
+++ behaviour-tree/godot-project/ai/TraderAI.gd
@@ -8,3 +8,6 @@ onready var ship = get_node(ship_path)
 onready var stash_area = get_node(stash_area_path)
 onready var boat_dock_position = get_node(boat_dock_position_path)
 
+func _ready():
+	# ensure _ready() has been finished everywhere else
+	yield(get_tree().root, "ready")
